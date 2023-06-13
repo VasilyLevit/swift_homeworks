8dc1049648e44a0f18dd86ef51ba13aea98a1e4b
Vasily Levit <vasily.levit@gmail.com>
2023-06-13 21:14:17 +0800

    added tasks of homework2

---
+++ intro_hw2.playground/Contents.swift
@@ -0,0 +1,34 @@
+import Cocoa
+
+/* Написать функцию, которая на вход принимает число: сумма,
+ которую пользователь хочет положить на вклад, следующий
+ аргумент — это годовой процент, третий аргумент — это срок
+ функция возвращает сколько денег получит пользователь по итогу.*/
+
+func profit(deposit: Double, annualInterest: Double, term: Double) -> Double {
+    deposit + deposit*annualInterest*term
+}
+
+print(profit(deposit: 10_000.0, annualInterest: 0.1, term: 1.5))
+
+/* Создать перечисление, которое содержит 3 вида пиццы,
+ и создать переменные с каждым видом пиццы.*/
+
+enum Pizza: String {
+    case calzone = "Calzone"
+    case napoletana = "Napoletana"
+    case siciliana = "Siciliana"
+}
+
+var calzone = Pizza.calzone
+var napoletana = Pizza.napoletana
+var siciliana: Pizza = .siciliana
+
+/* Добавить возможность получения названия пиццы через rawValue*/
+
+func getName(pizza: Pizza) -> String {
+    pizza.rawValue
+}
+
+var pizza_name = getName(pizza: .calzone)
+print(pizza_name)
